b2dfaee602e0ad2ba042f6895d4332d510fd1cb7
Mason Edwards <edwardsmason2000@gmail.com>
2022-11-22 11:16:23 +0000

    Added dashbord and done some cleaning

--- ComputingProject/DataGeneratingDevice/Program.cs
+++ ComputingProject/DataGeneratingDevice/Program.cs
@@ -16,9 +16,11 @@ using (var producer = new ProducerBuilder<Null, string>(config).Build())
     for (var i = 0; i < 1000; i++)
     {
 
-        String data = Convert.ToString(new Random().Next(1, 300));
-        TelemetryData telemetryData = new TelemetryData { Channel="vCar", Value=data};
-        String json = JsonSerializer.Serialize(telemetryData);
+        string x = Convert.ToString(new Random().Next(1, 300));
+        string y = Convert.ToString(new Random().Next(1, 300));
+        string z = Convert.ToString(new Random().Next(1, 300));
+
+        string data = $"{{\"x\": {x}, \"y\": {y}, \"z\": {z}}}";
 
         var result = await producer.ProduceAsync("TelemetryData", new Message<Null, string> { Value = $"{data}", Timestamp = new Timestamp(DateTimeOffset.Now) });
         Console.WriteLine($"{result.Message.Timestamp.UtcDateTime} : {result.Message.Value}");
@@ -26,11 +28,4 @@ using (var producer = new ProducerBuilder<Null, string>(config).Build())
 
         await Task.Delay(20);
     }
-}
-
-public class TelemetryData
-{
-    public string? Channel { get; set; }
-
-    public string? Value { get; set; }
 }
