9a4989391f47cf970f6ea0abdcd4808e74d363c4
Mason Edwards <edwardsmason2000@gmail.com>
2022-11-24 16:50:49 +0000

    Added chart that updates live with simulated data.
    Values like how often to create a data point, how often to update the chart, etc. have not been extensively tested.

---
+++ ComputingProject/Dashboard/Shared/Models/Parameter.cs
@@ -0,0 +1,42 @@
+﻿namespace Dashboard.Shared.Models
+{
+    /// <summary>
+    /// Class to represent a "label" on the chart. Each label will be a different parameter on the chart.
+    /// This class will be serialised with the data of a parameter and passed via Javascript interop to the chart
+    /// library.
+    /// </summary>
+    public class Parameter
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="label"> The name of the parameter to be the label on the chart.</param>
+        /// <param name="borderWidth"> Width of the line on the chart.</param>
+        /// <param name="borderColor"> Color of the line on the chart.</param>
+        /// <param name="backgroundColor">Background color of datapoint and label on the chart.</param>
+        public Parameter(string? label, int? borderWidth, string? borderColor, string? backgroundColor)
+        {
+            Label = label;
+            BorderWidth = borderWidth;
+            BorderColor = borderColor;
+            BackgroundColor = backgroundColor;
+        }
+
+        /// <summary>
+        /// The label on the chart.
+        /// </summary>
+        public string? Label { get; set; }
+        /// <summary>
+        /// Width of the line on the chart.
+        /// </summary>
+        public int? BorderWidth { get; set; }
+        /// <summary>
+        /// Color of the line on the chart.
+        /// </summary>
+        public string? BorderColor { get; set; }
+        /// <summary>
+        /// Background color of datapoint and label on the chart.
+        /// </summary>
+        public string? BackgroundColor { get; set; }
+    }
+}
---
+++ ComputingProject/Dashboard/Shared/Models/ParameterData.cs
@@ -0,0 +1,29 @@
+﻿namespace Dashboard.Shared.Models
+{
+    /// <summary>
+    /// Class to represent a data point to be plotted on the graph.
+    /// </summary>
+    public class ParameterData
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="value">Value of the datapoint.</param>
+        /// <param name="timestamp">Timestamp that data point was created, in milliseconds from unix time.</param>
+        public ParameterData(string? value, long? timestamp)
+        {
+            Value = value;
+            Timestamp = timestamp;
+        }
+
+        /// <summary>
+        /// Timestamp of the data point.
+        /// </summary>
+        public long? Timestamp { get; set; }
+        /// <summary>
+        /// Value of the data point.
+        /// </summary>
+        public string? Value { get; set; }
+
+    }
+}
