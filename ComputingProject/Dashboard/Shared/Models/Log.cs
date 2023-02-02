9c44a3ba0a9a0f2eaf42350a184df87e54ad3d52
Mason Edwards <edwardsmason2000@gmail.com>
2023-02-02 18:40:25 +0000

    Added basic functionality to telemetry log

---
+++ ComputingProject/Dashboard/Shared/Models/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Dashboard.Shared.Models
+{
+    public class Log
+    {
+        public Log(Parameter parameter, string log, DateTimeOffset timestamp)
+        {
+            Parameter = parameter;
+            LogMessage = log;
+            Timestamp = timestamp;
+        }
+
+        public Parameter Parameter { get; set;}
+
+        public string LogMessage { get; set; }
+            
+        public DateTimeOffset Timestamp { get; set; }
+    }
+}
